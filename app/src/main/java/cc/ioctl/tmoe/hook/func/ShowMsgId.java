66f5e54a06bac21970eddf7726062b1d4d12f564
ushastoe <fedosey8@gmail.com>
2024-06-23 04:00:22 +0300

    Add Timestamp

--- app/src/main/java/cc/ioctl/tmoe/hook/func/ShowMsgId.java
+++ app/src/main/java/cc/ioctl/tmoe/hook/func/ShowMsgId.java
@@ -1,9 +1,14 @@
 package cc.ioctl.tmoe.hook.func;
 
+
 import android.text.TextPaint;
+import android.util.Log;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 import cc.ioctl.tmoe.base.annotation.FunctionHookEntry;
 import cc.ioctl.tmoe.hook.base.CommonDynamicHook;
@@ -29,6 +34,8 @@ public class ShowMsgId extends CommonDynamicHook {
         messageOwner.setAccessible(true);
         Field msgId = Initiator.loadClass("org.telegram.tgnet.TLRPC$Message").getDeclaredField("id");
         msgId.setAccessible(true);
+        Field msgDate = Initiator.loadClass("org.telegram.tgnet.TLRPC$Message").getDeclaredField("date");
+        msgDate.setAccessible(true);
         Class<?> kTheme = Initiator.loadClass("org.telegram.ui.ActionBar.Theme");
         Field chatTimePaint = kTheme.getDeclaredField("chat_timePaint");
         chatTimePaint.setAccessible(true);
@@ -36,19 +43,39 @@ public class ShowMsgId extends CommonDynamicHook {
         timeTextWidth.setAccessible(true);
         Field timeWidth = kChatMessageCell.getDeclaredField("timeWidth");
         timeWidth.setAccessible(true);
+        Field dateKey = Initiator.loadClass("org.telegram.messenger.MessageObject").getDeclaredField("dateKeyInt");
+        dateKey.setAccessible(true);
         HookUtils.hookAfterIfEnabled(this, measureTime, param -> {
+
             CharSequence time = (CharSequence) currentTimeString.get(param.thisObject);
+            assert time != null;
             Object messageObject = param.args[0];
             Object owner = messageOwner.get(messageObject);
-            int id = msgId.getInt(owner);
-            String delta = id + " ";
-            time = delta + time;
-            currentTimeString.set(param.thisObject, time);
+//            int id = msgId.getInt(owner);
+//            String delta = id + " ";
+//            time = delta + time;
+            int date = msgDate.getInt(owner);
+            String formattedTime = null;
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                LocalDateTime dateTime = LocalDateTime.ofEpochSecond(date, 0, ZoneOffset.ofHours(3));
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+                formattedTime = dateTime.format(formatter);
+            }
+            currentTimeString.set(param.thisObject, formattedTime);
+
+            assert formattedTime != null;
             TextPaint paint = (TextPaint) chatTimePaint.get(null);
-            assert paint != null;
-            int deltaWidth = (int) Math.ceil(paint.measureText(delta));
-            timeTextWidth.setInt(param.thisObject, deltaWidth + timeTextWidth.getInt(param.thisObject));
-            timeWidth.setInt(param.thisObject, deltaWidth + timeWidth.getInt(param.thisObject));
+            int lenght = (int) Math.ceil(paint.measureText(formattedTime));
+
+            timeTextWidth.setInt(param.thisObject, lenght);
+            timeWidth.setInt(param.thisObject, lenght);
+//            TextPaint paint = (TextPaint) chatTimePaint.get(null);
+//            assert paint != null;
+//            int deltaWidth = (int) Math.ceil(paint.measureText(delta));
+//            timeTextWidth.setInt(param.thisObject, deltaWidth + timeTextWidth.getInt(param.thisObject));
+//            timeWidth.setInt(param.thisObject, deltaWidth + timeWidth.getInt(param.thisObject));
+//
         });
         return true;
     }
